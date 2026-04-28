a3a9cffd1aa49c61d413ea868254ec0431def7ce
Jilu P Jose <jilupjose111@gmail.com>
2026-04-28 06:46:40 +0530

    Module Import

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel, Field
+
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+import pickle
+import os
+
