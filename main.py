da8105a24142a9467d9f8095a25e1550e4b45156
Jilu P Jose <jilupjose111@gmail.com>
2026-04-28 07:10:55 +0530

    FastAPI Endpoints and functions added

--- main.py
+++ main.py
@@ -61,6 +61,78 @@ def load_model():
 load_model()
 
 
+class IrisInput(BaseModel):
+    sepal_length: float = Field(..., gt=0)
+    sepal_width: float = Field(..., gt=0)
+    petal_length: float = Field(..., gt=0)
+    petal_width: float = Field(..., gt=0)
+
+
+
+# FastAPI Routes
+
+@app.get("/")
+def home():
+    return {"message": "Iris API Running"}
+
+
+@app.get("/model-info")
+def model_info():
+    return {
+        "model": "RandomForestClassifier",
+        "dataset": "Iris Flowers",
+        "classes": iris.target_names.tolist(),
+        "accuracy": accuracy,
+    }
+
+@app.get("/health")
+def health():
+    return {"status": "OK"}
+
+
+
+@app.post("/predict")
+def predict(data: IrisInput):
+    if model is None:
+        raise HTTPException(status_code=500, detail="Model not loaded")
+    
+    input_data = [[
+        data.sepal_length,
+        data.sepal_width,
+        data.petal_length,
+        data.petal_width
+    ]]
+
+    pred = model.predict(input_data)[0]
+    prob = model.predict_proba(input_data)[0].max()
+
+    return {"prediction": iris.target_names[pred], "confidence": round(float(prob), 3)}
+
+
+@app.post("/predict-batch")
+def predict_batch(data: list[IrisInput]):
+    res = []
+
+    for item in data:
+        input_data=[[
+            item.sepal_length,
+            item.sepal_width,
+            item.petal_length,
+            item.petal_width,
+        ]]
+
+        pred = model.predict(input_data)[0]
+        res.append(iris.target_names[pred])
+    return {"predictions": res}
+
+
+@app.get("/retrain")
+def retrain():
+    train_model()
+    return {"message": "Model RETRAINED", "accuracy": accuracy}
+
+
+
 
 
 
