b81a896ba4d2387ef284a746ad7ac039b1e14b5b
Jilu P Jose <jilupjose111@gmail.com>
2026-04-28 06:57:47 +0530

    Functions added: train_model(), load_model()

--- main.py
+++ main.py
@@ -24,5 +24,46 @@ app.add_middleware(
 MODEL_PATH = "model.pkl"
 
 
+# Model Training
+
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+model = None
+accuracy = None
+
+
+def train_model():
+    global model, accuracy
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=20)
+
+    modle = RandomForestClassifier(n_estimators=100)
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+
+    with open(MODEL_PATH, "wb") as f:
+        pickle.dump(model, f)
+
+
+def load_model():
+    global model
+
+    if os.path.exists(MODEL_PATH):
+        with open(MODEL_PATH, "rb") as f:
+            model = pickle.load(f)
+    else:
+        train_model()
+
+load_model()
+
+
+
+
+
+
 
 
